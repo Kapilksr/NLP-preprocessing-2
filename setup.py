d9678fa1cc131cdcf548d79d3ffa10143d178c34
Kapil Singh <kapilsingh236142@gmail.com>
2021-10-23 18:02:53 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='preprocess_KS', ## this should be unique
+	version='0.2',
+	author='Kapil Singh',
+	author_email='kapilsingh236142@gmail.com',
+	description='This is text preprocessing tool',
+	packages=['preprocess_KS'],
+	url='https://github.com/Kapilksr/NLP-preprocessing-2.git',
+	python_requires='>=3.5'
+)
